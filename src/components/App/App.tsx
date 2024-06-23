1cf1b385ff6aca26b66850d38f0d2a8978e70d1b
dmycse <dmycse@gmail.com>
2024-06-23 10:15:07 +0300

    Refctor App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,18 @@
+import { CssBaseline, Stack } from "@mui/material";
+import Navbar from "../Header/Navbar";
+import Hero from "../Hero/Hero";
+import Form from "../Form/Form";
 
 function App() {
   
 
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <CssBaseline />
+      <Navbar />
+      <Hero />
+      <Form />
+    </>
   )
 }
 
